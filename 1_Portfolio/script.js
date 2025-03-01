6edf4aaf58750c7e7c72908ed9157b385ef966ac
ANNN <10717798@polimi.it>
2025-03-01 19:43:03 +0100

    Comment

--- 1_Portfolio/script.js
+++ 1_Portfolio/script.js
@@ -5,9 +5,15 @@ const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
+    //if the navLinks has this class 'open', remove it, otherwise add it
   navLinks.classList.toggle("open");
 
   const isOpen = navLinks.classList.contains("open");
+
+//modify the class of the icon, using the remixicon class 
+//setAttribute(name, value) sets the value of an attribute on the specified element. If the attribute already exists,
+//  the value is updated; otherwise a new attribute is added with the specified name and value.
+// an attribute can be any valid HTML attribute for the element, such as src, href, class, etc.
   menuBtnIcon.setAttribute(
     "class",
     // If the navLinks is open, show the close icon, otherwise show the menu icon
