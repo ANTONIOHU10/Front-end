38cf0cf4a4536e4d651362cb68f074149663586f
ANNN <10717798@polimi.it>
2025-03-02 21:30:03 +0100

    Effects

--- 1_Portfolio/script.js
+++ 1_Portfolio/script.js
@@ -28,3 +28,55 @@ navLinks.addEventListener("click", (e) => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-3-line");
 });
+
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  ScrollReveal().reveal(".header_content h2", {
+    ...scrollRevealOption,
+    delay: 200,
+  });
+  ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+    delay: 400,
+  });
+  ScrollReveal().reveal(".header_content p", {
+    ...scrollRevealOption,
+    delay: 700,
+  });
+  ScrollReveal().reveal(".header_content h4", {
+    ...scrollRevealOption,
+    delay: 1200,
+  });
+  ScrollReveal().reveal(".header_content .socials li", {
+    ...scrollRevealOption,
+    delay: 2000,
+    interval: 200,
+  });
+  ScrollReveal().reveal(".header_btns", {
+    ...scrollRevealOption,
+    delay: 3000,
+  });
+
+  ScrollReveal().reveal(".menu_container", {
+    ...scrollRevealOption,
+    delay: 3200,
+  });
+
+
+document.addEventListener('mouseover', (e) => {
+    const sidebar = document.querySelector('.side_bar_container');
+    const trigger = document.getElementById('sidebar_more');
+    
+    if (e.target.closest('.sidebar_more')) {
+            sidebar.style.right = '0';
+         
+    } else if (!e.target.closest('.side_bar_container')) {
+            sidebar.style.right = '-100%';
+            trigger.style.right = '100%';
+    }
+});
