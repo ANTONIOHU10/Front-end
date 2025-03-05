ef572d797d0bd651e39f6715614ea4e58fc7d0f1
ANNN <10717798@polimi.it>
2025-03-05 15:45:24 +0100

    Init

---
+++ 4_DownWard_sideBar/script.js
@@ -0,0 +1,30 @@
+const toggleButton = document.getElementById("toggle-button");
+const sidebar = document.getElementById("sidebar");
+
+const openIcon = toggleButton.querySelector("#arrow_downward");
+const closeIcon = toggleButton.querySelector("#arrow_upward");
+
+closeIcon.style.display = "none";
+
+
+//the toggle button
+toggleButton.addEventListener("click", () => {
+
+    toggleButton.classList.toggle("active");
+
+    if (toggleButton.classList.contains("active")){
+        toggleButton.style.top = "100px";
+    } else {
+        toggleButton.style.top = "50px";
+    }
+
+    sidebar.classList.toggle("active");
+
+    if (sidebar.classList.contains("active")) {
+        openIcon.style.display = "none";
+        closeIcon.style.display = "block";
+  } else {
+        openIcon.style.display = "block";
+        closeIcon.style.display = "none";
+  }
+});
