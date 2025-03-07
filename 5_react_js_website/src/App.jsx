c487852a9804b4ca6fb457b09e7881f0b5560c51
ANNN <10717798@polimi.it>
2025-03-07 11:35:47 +0100

    add job page init

--- 5_react_js_website/src/App.jsx
+++ 5_react_js_website/src/App.jsx
@@ -17,7 +17,8 @@ import HomePage from './Pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './Pages/JobsPage'
 import NotFoundPage from './Pages/NotFoundPage'
-import JobPage from './Pages/JobPage'
+import JobPage, {jobLoader} from './Pages/JobPage'
+import AddJobPage from './Pages/AddJobPage'
 
 
 const router = createBrowserRouter(
@@ -26,8 +27,9 @@ const router = createBrowserRouter(
     <Route path = "/" element={<MainLayout/>}>
           <Route index element={<HomePage/>}/>
           <Route path = "/jobs" element={<JobsPage/>}/>
+          <Route path = "/add-job" element= {<AddJobPage/>}/>
           {/** :id = dynamical for every pin job */}
-          <Route path = "/jobs/:id" element ={<JobPage/>}/>
+          <Route path = "/jobs/:id" element ={<JobPage/>} loader = {jobLoader}/>
           {/**all not found page -> path = "*" */}
           <Route path ="*" element={<NotFoundPage/>}/>
           
@@ -38,7 +40,7 @@ const router = createBrowserRouter(
 
 const App = () => {
 
-      return <RouterProvider router = {router}/>;
+      return <RouterProvider router = {router} />;
 
 };
 
