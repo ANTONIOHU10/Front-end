c4a29dea8d5c518ee4e5d5d02b44beff240eccdf
ANNN <10717798@polimi.it>
2025-03-08 23:05:41 +0100

    update

--- 6_react_hero_ui/src/Pages/HomePage.jsx
+++ 6_react_hero_ui/src/Pages/HomePage.jsx
@@ -7,9 +7,6 @@ import ViewAllJobs from '../components/ViewAllJobs'
 
 const HomePage = () => {
 
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    
   return (
     <>
         <Hero/>
