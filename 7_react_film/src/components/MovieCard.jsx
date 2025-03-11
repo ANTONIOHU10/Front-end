eb12deb01af7ba4bbf9bef875656185a7a22148e
ANNN <10717798@polimi.it>
2025-03-11 08:35:22 +0100

    fix images

--- 7_react_film/src/components/MovieCard.jsx
+++ 7_react_film/src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ const MovieCard = ({movie:{title, vote_average, poster_path, release_date,origin
     <div className='movie-card'>
 
         <img src ={poster_path ? 
-            `https://image.tmdb.org/t/p/w500/${poster_path}` : './src/assets/no-movie.png'
+            `https://image.tmdb.org/t/p/w500/${poster_path}` : '/assets/no-movie.png'
         }
             alt={title}
         />
